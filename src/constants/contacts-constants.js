b658c786f2e827038fdc7727bc9e43b584889d05
yurii <robka4an@gmail.com>
2024-07-03 12:06:15 +0300

    add tokens

--- src/constants/contacts-constants.js
+++ src/constants/contacts-constants.js
@@ -10,3 +10,6 @@ export const contactsFieldList = [
 ];
 
 export const typeList = ['work', 'home', 'personal'];
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 import { loginUser, registerUser } from '../services/auth.js';
-
+import { ONE_MONTH } from '../constants/contacts-constants.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
@@ -13,9 +13,22 @@ export const registerUserController = async (req, res) => {
 };
 
 export const loginUserController = async (req, res) => {
-  await loginUser(req.body);
+  const session = await loginUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_MONTH),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_MONTH),
+  });
 
   res.json({
     status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
