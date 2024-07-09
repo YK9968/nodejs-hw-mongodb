b5daa4488f589f9d30d8f485f6075af2f0230545
yurii <robka4an@gmail.com>
2024-07-09 16:24:20 +0300

    finish 1 part HW

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,11 +3,12 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 import { SMTP } from '../constants/contacts-constants.js';
 import { env } from './env.js';
+import createHttpError from 'http-errors';
 
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
-  secure: true,
+  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
@@ -15,5 +16,13 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  try {
+    const send = await transporter.sendMail(options);
+    return send;
+  } catch (err) {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 };
